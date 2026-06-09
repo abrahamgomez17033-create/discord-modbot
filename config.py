0113f87d6a85f704dee149c5fbbccd377095f75e
abrahamgomez17033-create <abrahamgomez17033@gmail.com>
2026-06-08 21:02:28 -0600

    Add config with env var for token

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+TOKEN = os.getenv("TOKEN", "PON_TU_TOKEN_AQUI")
+PREFIX = "!"
+LOG_CHANNEL_ID = None
+MUTE_ROLE_NAME = "Muted"
+TICKET_CATEGORY_NAME = "Tickets"
+STAFF_ROLES = ["Admin", "Mod", "Staff"]
